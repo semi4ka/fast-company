25cb9d65d9b938b22f26a060543e8749a284c59d
Sergii Sukhoi <mayulka@Sergiis-MacBook-Pro.local>
2021-08-25 19:24:40 +0300

    FIX renderPhrase

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -13,57 +13,63 @@ const Users = () => {
     if (number === 0)
       return <span className="badge bg-danger">Никто с тобой не тусанет</span>;
     return (
-      <>
-        <span className="badge bg-primary">
-          {number} человек{number <= 4 && number != 1 ? "a" : ""} тусанет с
-          тобой сегодня
-        </span>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col"> Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user => (
-              <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>
-                  {user.qualities.map(item => (
-                    <span
-                      key={item.color}
-                      className={"me-2 badge bg-" + item.color}
-                    >
-                      {item.name}
-                    </span>
-                  ))}
-                </td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate}</td>
-                <td>
-                  <button
-                    onClick={() => {
-                      handleDelete({ id: user._id });
-                    }}
-                    className="btn btn-danger"
-                  >
-                    delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
+      <span className="badge bg-primary">
+        {number} человек{number <= 4 && number != 1 ? "a" : ""} тусанет с тобой
+        сегодня
+      </span>
     );
   };
-  return <>{renderPhrase(users.length)}</>;
+  return (
+    <>
+      {renderPhrase(users.length)}
+
+      <table
+        className="table"
+        style={users.length === 0 ? { display: "none" } : {}}
+      >
+        <thead>
+          <tr>
+            <th scope="col"> Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>
+                {user.qualities.map(item => (
+                  <span
+                    key={item.color}
+                    className={"me-2 badge bg-" + item.color}
+                  >
+                    {item.name}
+                  </span>
+                ))}
+              </td>
+              <td>{user.profession.name}</td>
+              <td>{user.completedMeetings}</td>
+              <td>{user.rate}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    handleDelete({ id: user._id });
+                  }}
+                  className="btn btn-danger"
+                >
+                  delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Users;
