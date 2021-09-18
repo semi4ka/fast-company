ca057143db9cd5290bbcf6343f97ef9e3841c34f
Sergii Sukhoi <mayulka@Sergiis-MacBook-Pro.local>
2021-09-18 14:13:30 +0300

    final fix ex 5

--- src/api/fake.api/professions.api.js
+++ src/api/fake.api/professions.api.js
@@ -17,7 +17,7 @@ export const professions = [
 const fetchAll = () =>
     new Promise(resolve => {
         window.setTimeout(() => {
-            resolve(professions);
+            resolve(professionsObject);
         }, 2000);
     });
 
